fe936d0e3708795735a55a600e6ec420a7e27b95
zhouyuyan <joy.zhou@honeywell.com>
2017-08-22 16:58:50 +0800

    packet

--- broker/client.go
+++ broker/client.go
@@ -1,15 +1,28 @@
 package broker
 
-import "net"
+import (
+	"fhmq/lib/message"
+	"net"
+	"strings"
+)
 
 type client struct {
 	broker   Broker
 	conn     net.Conn
+	info     info
+	localIP  string
+	remoteIP string
+}
+
+type info struct {
 	clientID []byte
 	username []byte
 	password []byte
+	keeplive uint16
+	willMsg  *message.PublishMessage
 }
 
-func init() {
-
+func (c *client) init() {
+	c.localIP = strings.Split(c.conn.LocalAddr().String(), ":")[0]
+	c.remoteIP = strings.Split(c.conn.RemoteAddr().String(), ":")[0]
 }
