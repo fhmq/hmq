ef252550dc26cc1a82dc7a02b0858b7a85178147
Marc Magnin <marc.magnin@gmail.com>
2018-01-26 06:51:36 +0100

    fhmq/hmq#5 added zap logger (#11)

---
+++ logger/logger_test.go
@@ -0,0 +1,33 @@
+/* Copyright (c) 2018, joy.zhou <chowyu08@gmail.com>
+ */
+package logger
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.uber.org/zap"
+)
+
+func TestGet(t *testing.T) {
+	var l *zap.Logger
+	logger := Get()
+
+	assert.NotNil(t, logger)
+	assert.IsType(t, l, logger)
+}
+
+func TestNewDevLogger(t *testing.T) {
+	logger, err := NewDevLogger()
+
+	assert.Nil(t, err)
+	assert.True(t, logger.Core().Enabled(zap.DebugLevel))
+}
+
+func TestNewProdLogger(t *testing.T) {
+	logger, err := NewProdLogger()
+
+	assert.Nil(t, err)
+	assert.False(t, logger.Core().Enabled(zap.DebugLevel))
+}
